f0b16cf8347a54de06799a5cb483694498c99098
Aditya Misra <misraa@tcd.ie>
2018-10-25 00:57:58 +0100

    compaction

--- src/DataProcessing.py
+++ src/DataProcessing.py
@@ -3,6 +3,7 @@ import re
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 DATA_DIR = "data"
 
@@ -10,22 +11,18 @@ DATA_DIR = "data"
 def load_data():
     return pd.read_csv("../dataset/gender-classifier-DFE-791531.csv", encoding='latin1')
 
-def select_rows(df):
-    return df[df["gender"].isin(["male", "female"]) &
-              (df["gender:confidence"] > 0.99)].index.tolist()
+def split_data():
+    data = pd.read_csv("../dataset/gender-classifier-DFE-791531.csv", encoding='latin1')
+    array = ['male', 'female']
+    data1 = data.loc[(data['gender:confidence'] == 1) & data['gender'].isin(array)]
 
-def split_data(rows):
-    n_samples = len(rows)
-    random.shuffle(rows)
-    test_size = round(n_samples * 0.2)
-    test_rows = rows[:test_size]
-    train_rows = rows[test_size:]
-    m = len(test_rows)
-    n = len(train_rows)
-    df1 = pd.DataFrame(np.array(test_rows).reshape(m,1))
-    df2 = pd.DataFrame(np.array(train_rows).reshape(n,1))
-    df1.to_csv ('../output-data/test_rows.txt', index = None, header=True)
-    df2.to_csv ('../output-data/train_rows.txt', index = None, header=True)
+    for rows in data1:
+
+        x_train, x_test = train_test_split(data1, test_size=0.2)
+
+    x_train.to_csv('../output-data/train_rows.csv', index=None, header=True)
+    x_test.to_csv('../output-data/test_rows.csv', index=None, header=True)
+    return x_train, x_test
 
 def encode_class_labels(train_rows, test_rows, df):
     encoder = LabelEncoder()
@@ -49,13 +46,6 @@ def normalize_text(text):
 
     return text
 
-def load_data_split():
-    train = pd.read_csv("{}/train_rows.txt".format(DATA_DIR)).row_number.tolist()
-    test = pd.read_csv("{}/test_rows.txt".format(DATA_DIR)).row_number.tolist()
-
-    return train, test
-
 df = load_data()
-selected_rows = select_rows(df)
 
-split_data(selected_rows)
+split_data()
