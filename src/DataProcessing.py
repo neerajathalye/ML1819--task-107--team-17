364b29c5974edd0cbbf47f7459557c260f11bb8a
Aditya Misra <misraa@tcd.ie>
2018-10-25 19:27:30 +0100

    feature extraction

--- src/DataProcessing.py
+++ src/DataProcessing.py
@@ -1,9 +1,13 @@
-import random
 import re
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
 
 DATA_DIR = "data"
 
@@ -50,6 +54,110 @@ def normalize_text(text):
     text = re.sub('\s+', ' ', text)
 
     return text
+
+
+def compute_text_feats_test(vectorizer, df):
+    return vectorizer.transform(df["text_norm"])
+
+
+def compute_text_feats_train(vectorizer, df):
+    return vectorizer.transform(df["text_norm"])
+
+
+def compute_text_desc_feats_test(vectorizer, df):
+    train_text = df["text_norm"]
+    train_desc = df["description_norm"]
+
+    return vectorizer.transform(train_text.str.cat(train_desc, sep=' '))
+
+
+def compute_text_desc_feats_train(vectorizer, df):
+    train_text = df["text_norm"]
+    train_desc = df["description_norm"]
+
+    return vectorizer.transform(train_text.str.cat(train_desc, sep=' '))
+
+
+def extract_feats_from_text_test(df):
+    df["text_norm"] = [normalize_text(text) for text in df["text"]]
+    df["description_norm"] = [normalize_text(text) for text in df["description"].fillna("")]
+
+    vectorizer = CountVectorizer()
+    vectorizer = vectorizer.fit(df["text_norm"])
+
+    X_test = compute_text_feats_test(vectorizer, df)
+
+    return X_test
+
+
+def extract_feats_from_text_train(df):
+    df["text_norm"] = [normalize_text(text) for text in df["text"]]
+    df["description_norm"] = [normalize_text(text) for text in df["description"].fillna("")]
+
+    vectorizer = CountVectorizer()
+    vectorizer = vectorizer.fit(df["text_norm"])
+
+    X_train = compute_text_feats_test(vectorizer, df)
+
+    return X_train
+
+
+def extract_feats_from_text_and_desc_test(df):
+    df["text_norm"] = [normalize_text(text) for text in df["text"]]
+    df["description_norm"] = [normalize_text(text) for text in df["description"].fillna("")]
+
+    vectorizer = CountVectorizer()
+    train_text = df["text_norm"]
+    train_desc = df["description_norm"]
+    vectorizer = vectorizer.fit(train_text.str.cat(train_desc, sep=' '))
+
+    X_test = compute_text_desc_feats_test(vectorizer, df)
+
+    return X_test
+
+
+def extract_feats_from_text_and_desc_train(df):
+    df["text_norm"] = [normalize_text(text) for text in df["text"]]
+    df["description_norm"] = [normalize_text(text) for text in df["description"].fillna("")]
+
+    vectorizer = CountVectorizer()
+    train_text = df["text_norm"]
+    train_desc = df["description_norm"]
+    vectorizer = vectorizer.fit(train_text.str.cat(train_desc, sep=' '))
+
+    X_train = compute_text_desc_feats_test(vectorizer, df)
+
+    return X_train
+
+
+def extract_tweet_count_feats_test(df):
+    feats_test = df[["retweet_count", "tweet_count", "fav_number"]]
+
+    scaler = StandardScaler().fit(feats_test)
+
+    return (scaler.transform(feats_test))
+
+
+def extract_tweet_count_feats_train(df):
+    feats_train = df[["retweet_count", "tweet_count", "fav_number"]]
+
+    scaler = StandardScaler().fit(feats_train)
+
+    return (scaler.transform(feats_train))
+
+def print_results(y_true, y, X, data_set_name, class_names):
+    print(data_set_name)
+    print(classification_report(y, y_true, target_names=class_names))
+    print("Accuracy: {}".format(accuracy_score(y, y_true)))
+    print("==================================================================")
+    print()
+
+
+def report_results(grid_search, y_train, X_train, y_test, X_test, class_names):
+    print("Best params: ", grid_search.best_params_)
+    print_results(grid_search.predict(X_train), y_train, X_train, "Train", class_names)
+    print_results(grid_search.predict(X_test), y_test, X_test, "Train", class_names)
+
 
 df = load_data()
 
