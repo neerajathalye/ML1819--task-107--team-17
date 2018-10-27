f918f0047161acd7bee86448312a5a2577121d3d
Mr.Shubham <shubamkhanna@gmail.com>
2018-10-27 12:16:13 +0000

    SVM update=2
    Signed-off-by: Mr.Shubham <shubamkhanna@gmail.com>

--- src/classifier/SVMclass.py
+++ src/classifier/SVMclass.py
@@ -9,11 +9,10 @@ from sklearn.metrics import classification_report
 #Creating a classifier for SVM
 
 data=dp.load_data()
-JOBS=3
+JOBS=4
 PARAMS=[{ 'C': [4, 2, 1.5, 1, 0.5, 0.1, 0.05, 0.01, 0.001, 0.0001],
            'kernel': ["linear", "poly", "rbf", "sigmoid"],
-           'cache_size': [1000]}]
-           #'gamma': ['auto', 1.0, 1.0e-1, 1.0e-2, 1.0e-3, 1.0e-4, 1.0e-5, 1.0e-6]}]
+           'cache_size': [1000],'gamma': ['auto', 1.0, 1.0e-1, 1.0e-2, 1.0e-3, 1.0e-4, 1.0e-5, 1.0e-6]}]
 x_train, x_test = dp.split_data()
 
 X_train = dp.extract_feats_from_text(x_train)
