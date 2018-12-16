fce8776daf28fd5a46db962cbeba9007f7bc5143
Mr.Shubham <shubamkhanna@gmail.com>
2018-12-16 17:52:58 +0000

    Removed unused imports

--- src/Assignment 2/DataProcessing.py
+++ src/Assignment 2/DataProcessing.py
@@ -10,18 +10,12 @@ from collections import Counter
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
-
-#from sklearn_pandas import DataFrameMapper # Notice that this is actually Sklearn-Pandas library
-#%matplotlib inline
 
 # Load dataset
 dataset_path = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'dataset\\gender-classifier-DFE-791531.csv')
